fff6caa0f2f8f3a242d377f626b172dd6c9bac5d
jcesarmobile <jcesarmobile@gmail.com>
2021-11-15 18:36:07 +0100

    Add template app

---
+++ template/ios/template/ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
+    override func viewDidLoad() {
+    super.viewDidLoad()
+        let request = URLRequest(url: URL(string: "customscheme://localhost/index.html")!)
+        _ = webView?.load(request)
+    }
+
+    override func loadView() {
+        let webViewConfiguration = WKWebViewConfiguration()
+        webViewConfiguration.setURLSchemeHandler(WebViewHander(), forURLScheme: "customscheme")
+        webViewConfiguration.allowsInlineMediaPlayback = true
+        webViewConfiguration.mediaTypesRequiringUserActionForPlayback = []
+        webView = WKWebView(frame: .zero, configuration: webViewConfiguration)
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+}
+
